2aa01c013bd41f01f0bc71e676b39743abc45754
Umbarje <Akash.Umbarje@emtecinc.com>
2021-09-14 12:38:32 +0530

    myproject

---
+++ Contracts/ICompanyRepository.cs
@@ -0,0 +1,20 @@
+﻿using OneMoreNewDapper.Dto;
+using OneMoreNewDapper.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneMoreNewDapper.Contracts
+{
+   public interface ICompanyRepository
+    {
+        public Task<IEnumerable<Company>> GetCompanies();
+        public Task<Company> GetCompany(int id);
+        public Task<Company> CreateCompany(CompanyForCreationDto company);
+
+        public Task UpdateCompany(int id, CompanyForUpdateDto company);
+        public Task DeleteCompany(int id);
+
+    }
+}
